1a212f80a0171df2fe335af3d14be5083048b941
cdsurfer0212 <cdsurfer0212@gmail.com>
2018-08-14 01:53:17 +0800

    use placeholder for empty poster path

--- ShopBackAssignment/Movie/Cell/MovieListCollectionViewCell.swift
+++ ShopBackAssignment/Movie/Cell/MovieListCollectionViewCell.swift
@@ -29,7 +29,9 @@ class MovieListCollectionViewCell: UICollectionViewCell {
     var posterPath: String? {
         didSet {
             if let posterPath = posterPath, posterPath.count > 0 {
-                posterImageView.sd_setImage(with: URL(string: "https://image.tmdb.org/t/p/w300_and_h450_bestv2/\(posterPath)"))
+                posterImageView.sd_setImage(with: URL(string: "https://image.tmdb.org/t/p/w300_and_h450_bestv2/\(posterPath)"), placeholderImage: UIImage.init(named: "Icon-Placeholder"))
+            } else {
+                posterImageView.image = UIImage.init(named: "Icon-Placeholder")
             }
         }
     }
